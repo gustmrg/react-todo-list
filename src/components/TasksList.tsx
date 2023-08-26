e64eb732475bbb563aba36f4735a05a5519047d3
Gustavo Miranda <gmiranda90@outlook.com>
2023-08-26 14:17:11 -0300

    Add tasks info

--- src/components/TasksList.tsx
+++ src/components/TasksList.tsx
@@ -9,13 +9,27 @@ export function TasksList() {
 
   if (tasksList.length == 0) {
     return (
-      <div className={styles.container_tasks}>
-        <img src={clipboardImage} alt="Imagem de uma prancheta" />
-        <div>
-          <h4>Você ainda não tem tarefas cadastradas</h4>
-          <p>Crie tarefas e organize seus itens a fazer</p>
+      <>
+        <div className={styles.tasks_info}>
+          <div className={styles.tasks_created}>
+            <p>
+              Tarefas criadas <span>0</span>
+            </p>
+          </div>
+          <div className={styles.tasks_done}>
+            <p>
+              Tarefas concluídas<span>0</span>
+            </p>
+          </div>
         </div>
-      </div>
+        <div className={styles.container_tasks}>
+          <img src={clipboardImage} alt="Imagem de uma prancheta" />
+          <div>
+            <h4>Você ainda não tem tarefas cadastradas</h4>
+            <p>Crie tarefas e organize seus itens a fazer</p>
+          </div>
+        </div>
+      </>
     );
   }
 }
