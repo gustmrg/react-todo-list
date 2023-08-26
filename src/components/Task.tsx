a3516bf215fe66c84eebeaacfe3cfd1a9cdd2a2d
Gustavo Miranda <gmiranda90@outlook.com>
2023-08-26 20:32:40 -0300

    Add Task component

---
+++ src/components/Task.tsx
@@ -0,0 +1,28 @@
+import { Trash } from "@phosphor-icons/react";
+
+import styles from "./Task.module.css";
+
+interface Task {
+  id: string;
+  description: string;
+  isComplete: boolean;
+}
+
+export function Task(task: Task) {
+  return (
+    <div className={styles.task}>
+      <input type="checkbox" />
+      <p
+        className={
+          task.isComplete ? styles.description_done : styles.description
+        }
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime eveniet
+        et cupiditate dolor accusantium, optio dolorum asperiores! Dolor
+        possimus explicabo sequi aliquam, nihil, in modi tempora cumque, iusto
+        reiciendis odio?
+      </p>
+      <Trash size={48} className={styles.icon} />
+    </div>
+  );
+}
--- src/components/TasksList.tsx
+++ src/components/TasksList.tsx
@@ -1,9 +1,10 @@
+import { Task } from "./Task";
+
 import styles from "./TasksList.module.css";
 import clipboardImage from "../assets/Clipboard.png";
 
 export function TasksList() {
-  const tasksList: string[] = [];
-  console.log(tasksList.length);
+  const tasksList: string[] = ["Task 1", "Task 2"];
 
   if (tasksList.length == 0) {
     return (
@@ -20,12 +21,32 @@ export function TasksList() {
             </p>
           </div>
         </div>
-        <div className={styles.tasks_list}>
+        <div className={styles.tasks_list__empty}>
           <img src={clipboardImage} alt="Imagem de uma prancheta" />
           <div>
             <h4>Você ainda não tem tarefas cadastradas</h4>
             <p>Crie tarefas e organize seus itens a fazer</p>
           </div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className={styles.tasks}>
+        <div className={styles.tasks_info}>
+          <div className={styles.tasks_created}>
+            <p>
+              Tarefas criadas <span>0</span>
+            </p>
+          </div>
+          <div className={styles.tasks_done}>
+            <p>
+              Tarefas concluídas <span>0</span>
+            </p>
+          </div>
+        </div>
+        <div className={styles.tasks_list}>
+          <Task />
         </div>
       </div>
     );
